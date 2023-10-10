01e8cafcb0b0cebd02f2aa3b1591ab80ed2723ee
Abdelrhman Elmahdy <Abdelrahman.elmahdy@btech.com>
2023-10-10 13:46:22 +0300

    first commit

---
+++ CITest/CITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITestApp.swift
+//  CITest
+//
+//  Created by Abdelrhman Elmahdy on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
